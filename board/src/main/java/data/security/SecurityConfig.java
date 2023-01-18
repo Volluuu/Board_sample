0aa7afe8d655b7807289e80a0a5f3dc5b5e6601b
pjwoo <pjwoo@DESKTOP-2B578V5>
2023-01-18 17:52:34 +0900

    config 수정 2차

--- board/src/main/java/data/security/SecurityConfig.java
+++ board/src/main/java/data/security/SecurityConfig.java
@@ -1,73 +1,114 @@
 package data.security;
 
+import javax.servlet.http.HttpServletResponse;
 
-
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer.JwtConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import lombok.RequiredArgsConstructor;
 
 //웹 보안 어노테이션
 //추가적인 설정을 위해  extends WebSecurityConfigurerAdapter 추가
+@Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
-@Configuration(proxyBeanMethods = false)
-@ConditionalOnDefaultWebSecurity
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class SecurityConfig  {
-	
-		 
-	
-//	    @Bean
-//	    public PasswordEncoder passwordEncoder() {
-//	        return new BCryptPasswordEncoder();
-//	    }
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
-//	    @Bean
-//	    public void configure(WebSecurity web) {
-//	        web.ignoring()
-//	               .antMatchers("/h2-console/**", "/favicon.ico");
-//	    }
-
-//	    @Override
-//	    protected void configure(HttpSecurity http) throws Exception {
-//	        // CSRF 설정 Disable
-//	        http
-//             .csrf().disable();
-//	        http
-//	        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션을 사용하지 않을것으로 stateless처리한다
-//	        .and()
-////	        .addFilter()
-//	        .formLogin().disable()//폼로그인 방식의 로그인을 사용하지 않는다
-//	        .httpBasic().disable() //JWT는 httpBearer 방식으로 httpBasic방식은 disable처리
-//	        .authorizeRequests()
-//	        //이부분은 페이지에 맞게 작성
-//	        .antMatchers("/user/**")
-//	        .anyRequest()//그 이외에 모든 요청은 허용한다
-//	        .permitAll();
-//	    }
-
-//	    @Bean
-//	    public CorsConfigurationSource corsConfigurationSource() {
+//	private final JwtConfigurer jwtConfigure;
 //
-//	        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//	        corsConfiguration.addAllowedOrigin("http//localhost:3000");
-//	        corsConfiguration.addAllowedHeader("*");
-//	        corsConfiguration.addAllowedOriginPattern("*");
-//	        corsConfiguration.addAllowedMethod("*");
-//	        corsConfiguration.setAllowCredentials(true);
-//	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//	        source.registerCorsConfiguration("/**", corsConfiguration);
-//	        return source;
-//	    }
+//	public WebSecurityConfigure(JwtConfigurer jwtConfigure) {
+//		this.jwtConfigure = jwtConfigure;
+//	}
+//
+//	@Bean
+//	PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//
+//	@Bean
+//	public Jwt jwt() {
+//		return new Jwt(
+//			this.jwtConfigure.issuer(),
+//			this.jwtConfigure.clientSecret(),
+//			this.jwtConfigure.accessToken(),
+//			this.jwtConfigure.refreshToken()
+//		);
+//	}
+//
+//	public JwtAuthenticationFilter jwtAuthenticationFilter(Jwt jwt, TokenService tokenService) {
+//		return new JwtAuthenticationFilter(
+//			this.jwtConfigure.accessToken().header(),
+//			this.jwtConfigure.refreshToken().header(),
+//			jwt,
+//			tokenService
+//		);
+//	}
+//
+//	@Bean
+//	public AccessDeniedHandler accessDeniedHandler() {
+//		log.warn("accessDeniedHandler");
+//		return (request, response, e) -> {
+//			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+//			response.setContentType("text/plain;charset=UTF-8");
+//			response.getWriter().write("ACCESS DENIED");
+//			response.getWriter().flush();
+//			response.getWriter().close();
+//		};
+//	}
+//
+//	@Bean
+//	public AuthenticationEntryPoint authenticationEntryPoint() {
+//		return (request, response, e) -> {
+//			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//			response.setContentType("text/plain;charset=UTF-8");
+//			response.getWriter().write("UNAUTHORIZED");
+//			response.getWriter().flush();
+//			response.getWriter().close();
+//		};
+//	}
+//
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http, Jwt jwt, TokenService tokenService) throws
+//		Exception {
+//		http
+//			.authorizeRequests()
+//			.antMatchers("/api/members/signup", "/api/members/signin").permitAll()
+//			.anyRequest().authenticated()
+//			.and()
+//			.formLogin()
+//			.disable()
+//			.csrf()
+//			.disable()
+//			.headers()
+//			.disable()
+//			.httpBasic()
+//			.disable()
+//			.rememberMe()
+//			.disable()
+//			.logout()
+//			.disable()
+//			.sessionManagement()
+//			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//			.and()
+//			.exceptionHandling()
+//			.accessDeniedHandler(accessDeniedHandler())
+//			.authenticationEntryPoint(authenticationEntryPoint())
+//			.and()
+//			.addFilterBefore(jwtAuthenticationFilter(jwt, tokenService), UsernamePasswordAuthenticationFilter.class);
+//
+//		return http.build();
+//	}
 
 }
